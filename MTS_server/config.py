34a17377c8e0dacafc130daf54014dada5b87d2b
MinghuiZou <1359410672@qq.com>
2021-09-06 12:32:38 +0800

    修改构造症状List的方法

--- MTS_server/config.py
+++ MTS_server/config.py
@@ -1,12 +1,21 @@
 # 设置neo4j的配置信息
 NEO4J_CONFIG = {
-	"uri": "bolt://127.0.0.1:7687",
-	"auth": ("neo4j", "123456"),
-	"encrypted": False
+    "uri": "bolt://127.0.0.1:7687",
+    "auth": ("neo4j", "123456"),
+    "encrypted": False
 }
+
 
 # 读取txt文件返回list
 def txt2list(file):
-	with open(file, encoding="utf-8") as f:
-		content = f.read()
-		return content.split("\n")
+    with open(file, encoding="utf-8") as f:
+        content = f.read()
+        return content.split("\n")
+
+
+# 定义pipeline中可能出现的错误
+error_code_description = dict([
+    (1, "请输入有效的症状描述！"),  # description 为空
+    (2, "抱歉，我没有识别出有效的症状！"),  # symptoms 为空
+    (3, "抱歉，这超出我的知识范围了！")  # diseases 为空
+])
--- MTS_server/main_server.py
+++ MTS_server/main_server.py
@@ -1,6 +1,8 @@
+from os import error
 from flask import Flask
 from flask import request
 from neo4j import GraphDatabase
+from itertools import combinations
 from config import *
 import jieba
 jieba.load_userdict("./dict/symptom.txt")
@@ -14,10 +16,6 @@ flask_mode = True
 
 # 处理用户症状选择或文字输入
 def description_process(type, description):
-    if not description:
-        print("请输入有效的症状描述！")
-        return None
-
     if not type:  # 类型0，症状选择
         symptoms = description.split(",")
     else:  # 类型1，文字输入
@@ -29,22 +27,32 @@ def description_process(type, description):
                 symptoms.append(item)
             elif len(item) >= 2:
                 pass
+    symptoms = sorted(set(symptoms), key=symptoms.index)  # 去除重复项
     return symptoms
+
+
+# 构造症状List
+def get_symptoms_list(symptoms):
+    # # 按照前向最大匹配的思路构造症状List
+    # symptoms_list = []
+    # symptoms_list.append(symptoms)
+    # for index in range(len(symptoms) - 1, 0, -1):
+    #     symptoms_list.append(symptoms[:index])
+
+    # 按照从长到短的顺序，以组合的形式构造症状List
+    symptoms_list = []
+    for length in range(len(symptoms), 0, -1):
+        symptoms_list.extend(list(combinations(symptoms, length)))
+    return symptoms_list
 
 
 # 查询Neo4j图数据库
 def query_neo4j(symptoms):
-    if not symptoms:
-        print("请输入有效的症状描述！")
-        return None
+    # 构造症状List
+    symptoms_list = get_symptoms_list(symptoms)
+    print(f"\033[;36msymptoms_list: \033[0m{symptoms_list}")
 
-    # 按照前向最大匹配的思路构造症状List
-    symptoms_list = []
-    symptoms_list.append(symptoms)
-    for index in range(len(symptoms) - 1, 0, -1):
-        symptoms_list.append(symptoms[:index])
-
-    # 按照症状List查询图数据库
+    # 基于症状List查询图数据库
     for symptoms in symptoms_list:
         with neo4j_driver.session() as session:
             cypher = ""
@@ -61,29 +69,42 @@ def query_neo4j(symptoms):
             result = dict(zip(keys, result))
             if result:  # symptoms_list中前面的item优先级高，如果查询到结果就返回
                 print(f"\033[;36mquery_symptoms: \033[0m{symptoms}")
-                # print(f"query_result: {result}")
                 return result
     return None
 
 
 @app.route('/MTS_server/main_serve/', methods=["POST"])
 def main_serve():
-    print("\n\n")
     if flask_mode:
         type = request.form['type']  # 0代表症状选择，1代表文字输入
         description = request.form['description']  # 两种方式都以String形式传递，0类型用“,”分隔
     else:
         type = 1
-        description = "鼻塞情绪性感冒"
-        # description = "鼻塞情绪性感冒傻逼玩意鼻塞咳嗽"
+        # description = "鼻塞情绪性感冒"
+        description = "鼻塞情绪性感冒傻逼玩意鼻塞咳嗽"
         # description = "这是一个无效的症状描述！"
     print(f"\033[;36mdescription: \033[0m{description}")
+    # 判断当前pipeline的结果是否符合预期（description非空）
+    if not description:
+        print(error_code_description[1], end="\n\n")
+        return {1: error_code_description[1]}
+
     symptoms = description_process(type, description)
     print(f"\033[;36msymptoms: \033[0m{symptoms}")
+    # 判断当前pipeline的结果是否符合预期（symptoms非空）
+    if not symptoms:
+        print(error_code_description[2], end="\n\n")
+        return {2: error_code_description[2]}
+
     diseases = query_neo4j(symptoms)
     print(f"\033[;36mdiseases: \033[0m{diseases.keys()}")
-    print("\n\n")
-    return str(diseases.keys())
+    # 判断当前pipeline的结果是否符合预期（diseases非空）
+    if not diseases:
+        print(error_code_description[3], end="\n\n")
+        return {3: error_code_description[3]}
+
+    print("\n" + "-" * 52 + "\n")
+    return str(diseases)
 
 
 if __name__ == '__main__':
